79067faabb7f678aec92e5602c9c1afae27e8b1b
savi12 <savi.david@hotmail.com>
2014-04-21 20:08:14 +0200

    Application input language parse - method stubs

--- Application.java
+++ Application.java
@@ -1,77 +1,105 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
     private int mana;
-
+    
     public void run() {
         // TODO - implement Application.run
         throw new UnsupportedOperationException();
     }
 
     public static void main(String[] args) {
-        System.out.println("Which test case do you want to run(press number)?");
-        System.out.println("0: Exit");
-        System.out.println("1: Upgrade Building");
-        System.out.println("2: Build Trap");
-        System.out.println("3: Build Tower");
-        System.out.println("4: Tower Shoot");
-        System.out.println("5: Tower kills a Character");   // ezt folytassatok, meg
-        System.out.println("6: Move to trapped field");     // ertelemszeruen a switchcaset
-        System.out.println("7: Character Dies");            // is
-        System.out.println("8: Create Character");
-
-        Skeleton s = new Skeleton();
-
-        Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        while (i != 0) {
-            switch (i) {
-                case 1:
-                    s.upgradeBuilding();
-                    break;
-                case 2:
-                    s.buildTrap();
-                    break;
-                case 3:
-                    s.buildTower();
-                    break;
-                case 4:
-                    s.towerShoot();
-                    break;
-                case 5:
-                    s.towerKills();
-                    break;
-                case 6:
-                    s.move();
-                    break;
-                case 7:
-                    s.characterDies();
-                    break;
-                case 8:
-                    s.createCharacter();
-                    break;
-                default:
-                    System.out.println("Wrong number");
-                    break;
-            }
-            System.out.println();
-            System.out.println("0: Exit");
-            System.out.println("1: Upgrade Building");
-            System.out.println("2: Build Trap");
-            System.out.println("3: Build Tower");
-            System.out.println("4: Tower Shoot");
-            System.out.println("5: Tower kills a Character");
-            System.out.println("6: Move to trapped field");
-            System.out.println("7: Character Dies");
-            System.out.println("8: Create Character");
-            i = sc.nextInt();
-        }
-        sc.close();
+    	File file=new File("test"+args[0]+".txt");
+    	List<String> commands=readFile(file);
+    	//parsing the input language
+    	//and calling the appropriate test methods
+    	for (String cmd : commands) {
+			String[] temp=cmd.split(" ");
+			if(temp[0].equals("createGameField")){
+				createGameField(temp);
+			}
+			else if(temp[0].equals("createGameField")){
+				createGameField(temp);
+			}
+			else if(temp[0].equals("addRoad")){
+				addRoad(temp);
+			}else if(temp[0].equals("addChar")){
+				addChar(temp);
+			}else if(temp[0].equals("print")){
+				print(temp);
+			}else if(temp[0].equals("refresh")){
+				refresh(temp);
+			}else if(temp[0].equals("addTower")){
+				addTower(temp);
+			}else if(temp[0].equals("addTrap")){
+				addTrap(temp);
+			}else if(temp[0].equals("useStone")){
+				useStone(temp);
+			}else if(temp[0].equals("addStone")){
+				addStone(temp);
+			}else if(temp[0].equals("addFog")){
+				addFog(temp);
+			}
+			
+    	}
     }
+    //Functions for testing
 
-    public void init() {
+
+    private static void addFog(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void addStone(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void useStone(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void addTrap(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void addTower(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void refresh(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void print(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void addChar(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void addRoad(String[] temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void init() {
         // TODO - implement Application.init
         throw new UnsupportedOperationException();
     }
@@ -86,6 +114,38 @@ public class Application {
      */
     public void setMana(int mana) {
         this.mana = mana;
+    }
+    
+    private static void createGameField(String[] args){
+    	
+    }
+    
+    //Reads the lines of a file, and returns a list of them
+    private static List<String> readFile(File f){
+    	List<String> list = new ArrayList<String>();
+    	File file = new File("file.txt");
+    	BufferedReader reader = null;
+
+    	try {
+    	    reader = new BufferedReader(new FileReader(file));
+    	    String text = null;
+
+    	    while ((text = reader.readLine()) != null) {
+    	        list.add(text);
+    	    }
+    	} catch (FileNotFoundException e) {
+    	    e.printStackTrace();
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	} finally {
+    	    try {
+    	        if (reader != null) {
+    	            reader.close();
+    	        }
+    	    } catch (IOException e) {
+    	    }
+    	}
+    	return list;
     }
 
 }
