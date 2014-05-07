3de9f7a873367ebfec50a52fd501600a3940e325
szekike <gstplayer8@gmail.com>
2014-05-07 21:27:11 +0200

    amikor mi akarjuk

--- ApplicationForm.java
+++ ApplicationForm.java
@@ -143,7 +143,9 @@ public class ApplicationForm extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         //Start
-        
+        Canvas canvas = new Canvas();
+        this.setVisible(false);
+        canvas.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
